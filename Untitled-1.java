a098b3918c0300898a92d4baa175bda24e1590c6
derinh <da.amal@icloud.com>
2023-11-14 01:03:09 +0100

    transformed into separate function

--- Untitled-1.java
+++ Untitled-1.java
@@ -1,18 +1,24 @@
- class Vowel {
+class Vowel {
 
-    public static void main(String args[])
-    {
-    
-    String input = new String("hElloO");
-    String inputL = input.toLowerCase();
-    int counter = 0;
+    public static void main(String[] args){ 
+        String str = "HelloO1";  //Input String. Can be changed.
+        str = str.toLowerCase(); //convert everything to lowercase to avoid checking fo capitalized vowels too
+        char[] chars = str.toCharArray(); //necessary to hand in as parameter to counter function
+        int result = 0;
+        result = vowelCounter(chars); //get result from function
+        System.out.println("Number of vowels: " + result); //print out result
 
-for(int i=0; i<input.length(); i++){
-    if(inputL.charAt(i)=='a' || inputL.charAt(i)=='e' || inputL.charAt(i)=='i' || inputL.charAt(i)=='o' || inputL.charAt(i)=='u'){
-        counter++;
-    }
-}
+    } //end of main
 
-    System.out.println("Number of vowels in word: " + counter);
-}
+  //actual function to do the vowel-counting work
+    public static int vowelCounter(char[] chars) { 
+        int counter = 0; 
+        for(int i=0; i<chars.length; i++){
+            if(chars[i]=='a' || chars[i]=='e' || chars[i]=='i' || chars[i]=='o' || chars[i]=='u'){
+                counter++; //if vowel found, increase counter
+            }
+        } 
+        return counter; 
+    } //end of function
+
 } //end of class
